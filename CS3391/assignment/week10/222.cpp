1cd7d833e2087f417626f592015bc2e02cf11663
Kelvin Wu <wukuwnyu@gmail.com>
2020-02-23 12:29:10 +0800

    dev

--- CS3391/assignment/week10/222.cpp
+++ CS3391/assignment/week10/222.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <cstdlib>
+#include <sstream>
 
 
 using namespace std;
 
 void mFun() {
     string s;
+    cout << fixed << setprecision(4);
+    int cnt = 0;
     while(getline(cin, s)) {
+        if(cnt > 0)
+            cout << endl;
         long double sum = 0;
-        int n = s.length();
-        int i = 0;
-        while(i < n) {
-            int j = i;
-            bool f = false;
-            while(j < n && !isspace(s[j])) {
-                j++;
-                f = true;
-            }
-            if(f) {
-                sum += (long double) atof(s.substr(i, j).c_str());
-                i = j;
-            }else
-                i++;
+        stringstream ss;
+        ss << s;
+        long double a;
+        while(ss >> a) {
+            sum += a;
         }
-        cout << fixed << setprecision(4) << sum << endl;
-        cout << endl;
+       
+	    
+	    
+	    cout << sum << endl;
+	    cnt++;
     }
 }
 
