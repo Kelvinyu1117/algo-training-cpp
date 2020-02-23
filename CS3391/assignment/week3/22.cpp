205c72752e7e5badd60376fe5ba208612d7c95cb
Kelvinyu <wukuwnyu@gmail.com>
2020-02-23 16:49:51 +0800

    dev

--- CS3391/assignment/week3/22.cpp
+++ CS3391/assignment/week3/22.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#define N 10000
+#define N 10020
 using namespace std;
 struct State {
     int digit[4];
@@ -11,10 +12,8 @@ struct State {
 
 
 bool primes[N];
-bool v[N];
 void SoE() {
     std::fill_n(primes, N, true);
-    std::fill_n(v, N, false);
     
     for(int i = 2; i * i <= N; i++) {
         if(primes[i]) {
@@ -42,88 +41,64 @@ void mFun() {
     int t, s, d;
     
     while(cin >> t) {
-        cin >> s >> d;
-        if(s > d) { // assert s <= d
-            int tmp = s;
-            s = d;
-            d = tmp;
-        }
-        
-        State p;
-        p.num = s;
-        p.dist = 0;
-        setDigit(p);
-        queue<State> q;
-        q.push(p);
-        
-        bool hasPath = false;
-        
-        int res = 0;
-        while(!q.empty()) {
-            State curr = q.front(); q.pop();
-            
-            if(curr.num == d)
-            {
-                hasPath = true;
-                res = curr.dist;
-                break;
+        while(t--) {
+            cin >> s >> d;
+            if(s > d) { // assert s <= d
+                int tmp = s;
+                s = d;
+                d = tmp;
             }
             
+            bool v[N];
+            std::fill_n(v, N, false);
             
-            if(curr.num == 1033) {
-                int a = 10;
-            }
-             
-            if(curr.num == 1733) {
-                int a = 10;
-            }
+            State p;
+            p.num = s;
+            p.dist = 0;
+            setDigit(p);
+            queue<State> q;
+            q.push(p);
             
-            if(curr.num == 3379) {
-                int a = 10;
-            }
-             
-            if(curr.num == 3779) {
-                int a = 10;
-            }
+            bool hasPath = false;
             
-             
-            if(curr.num == 8779) {
-                int a = 10;
-            }
-             
-            if(curr.num == 8179) {
-                int a = 10;
-            }
-            if(!v[getPrimeCode(curr.digit)]) {
-                for(int i = 0; i < 4; i++) {
-                    for(int j = 0; j < 9; j++) {
-                        int tmp[4] = {curr.digit[0], curr.digit[1], curr.digit[2], curr.digit[3]};
-                        if(i == 3) {
-                            if(j != 0 && j != tmp[i]) {
-                                tmp[i] = j;
-                                if(primes[getPrimeCode(tmp)])
-                                    q.push({{tmp[0], tmp[1], tmp[2], tmp[3]}, getPrimeCode(tmp), curr.dist + 1});
-                            }
-                        }else {
-                            if(j != tmp[i]) {
-                                tmp[i] = j;
-                                if(primes[getPrimeCode(tmp)])
-                                    q.push({{tmp[0], tmp[1], tmp[2], tmp[3]}, getPrimeCode(tmp), curr.dist + 1});
-                            }
-                        }
-                    }
+            int res = 0;
+            while(!q.empty()) {
+                State curr = q.front(); q.pop();
+                
+                if(curr.num == d)
+                {
+                    hasPath = true;
+                    res = curr.dist;
+                    break;
                 }
                 
-                v[getPrimeCode(curr.digit)] = 1;
+                
+                if(!v[curr.num]) {
+                    vector<int> y;
+                    for(int i = 0; i < 4; i++) {
+                        for(int j = 0; j <= 9; j++) {
+                            int tmp[4] = {curr.digit[0], curr.digit[1], curr.digit[2], curr.digit[3]};
+                            if(i == 3 && j == 0) continue;
+                            
+                            tmp[i] = j;
+                            
+                            if(primes[getPrimeCode(tmp)] && !v[getPrimeCode(tmp)])
+                                q.push({{tmp[0], tmp[1], tmp[2], tmp[3]}, getPrimeCode(tmp), curr.dist + 1});
+                        }
+                    }
+                    
+                    int a = 10;
+                    
+                    v[curr.num] = 1;
+                }
+            }
+            
+            if(hasPath) {
+                cout << res << endl;
+            }else{
+                cout << "Impossible" << endl;
             }
         }
-        
-        if(hasPath) {
-            cout << res << endl;
-        }else{
-            cout << "Impossible" << endl;
-        }
-        
     }
 }
 
