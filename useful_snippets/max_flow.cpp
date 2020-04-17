04702e6ad83ffa1e26b0aa78632d87d3d1e27c70
Kelvinyu <wukuwnyu@gmail.com>
2020-04-17 15:17:17 +0800

    dev

--- useful_snippets/max_flow.cpp
+++ useful_snippets/max_flow.cpp
@@ -1,123 +1,66 @@
+#include <iostream>
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <iostream>
-
 #define INF 1e9
-#define N 30050
 using namespace std;
 
-struct Edge {
-    int next;
-    int prev;
-    int flow;
-    int cap;
+#define N 300
+int cap[N][N];
+int flow[N];
+int pre[N];
 
-    Edge(int n, int p, int f, int c): next(n), prev(p), flow(f), cap(c) {}; 
-};
-vector<vector<Edge>> g(N);
-vector<int> level(N);
-vector<int> nextNode(N);
-
-struct Dinic {    
-    void addEdge(int prev, int next, int weight) {
-        Edge forward = {next, g[next].size(), 0, weight};
-        Edge backward = {prev, g[prev].size(), 0, weight};
-
-        g[prev].push_back(forward);
-        g[next].push_back(backward);
-    }
-
-    void clear() {
-        for(int i = 0; i < N; i++) {
-            g.clear();
+int bfs(int s, int t, int n) {
+   
+    fill(pre, pre + n, -1);
+    queue<int> q;
+    q.push(s);
+    
+    flow[s] = INF;
+    
+    while(!q.empty()) {
+        int curr = q.front(); q.pop();
+        
+        if(curr == t) break;
+        
+        for(int i = 0; i < n; i++) {
+            if(curr == i) continue;
+            
+            if(pre[i] == -1 && cap[curr][i] > 0) {
+                flow[i] = min(flow[curr], cap[curr][i]);
+                pre[i] = curr;
+                q.push(i);
+            }
         }
-        level.clear();
-        nextNode.clear();
-        g.clear();
     }
     
-    bool bfs(int s, int e) {
-        fill(level.begin(), level.end(), -1);
-        
-        queue<int> q;
-        q.push(s);
-        level[s] = 0;
-
-        while(!q.empty()) {
-            int curr = q.front(); q.pop();
-
-            for(int i = 0; i < g[curr].size(); i++) {
-                Edge currEdge = g[curr][i];
-                int next = currEdge.next;
-
-                if(level[next] == -1 && currEdge.flow < currEdge.cap) {
-                    level[next] = level[curr] + 1;
-                    q.push(next);
-                }
-            }
-        }
-
-        return level[e] != -1;
+    if(pre[t] == -1) return 0;
+    
+    int dest = t;
+    
+    while(dest != s) {
+        int from = pre[dest];
+        cap[from][dest] -= flow[t];
+        cap[dest][from] += flow[t];
+        dest = from;
     }
-
-    int dfs(int s, int e, int currFlow) {
-        if(s == e)
-            return currFlow;
-
-        for(nextNode[s]; nextNode[s] < g[s].size(); nextNode[s]++) {
-            Edge &ed = g[s][nextNode[s]];
-
-            if(level[ed.next] == level[s] + 1 && ed.flow < ed.cap) {
-                int nextFlow = min(currFlow, ed.cap - ed.flow);
-                int dfs_val = dfs(ed.next, e, nextFlow);
-
-                if(dfs_val > 0) {
-                    ed.flow += dfs_val;
-                    g[ed.next][ed.prev].flow -= dfs_val;
-                    return dfs_val; 
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    int max_flow(int s, int e) {
-        int total_flow = 0;
-        
-        while(bfs(s, e)) {
-            int flow = dfs(s, e, INF);
-            fill(nextNode.begin(), nextNode.end(), 0);
-            while(flow) {
-                total_flow += flow;
-                flow = dfs(s, e, INF);
-            }
-        }
-
-        return total_flow;
-    }
-};
-
-
-void m() {
-    int n, m;
-    Dinic dinic;
-    while(cin >> n >> m) {
-        dinic.clear();
-        for(int i = 0; i < m; i++) {
-            int a, b, c;
-            cin >> a >> b >> c;
-            a--;
-            b--;
-            dinic.addEdge(a, b, c);
-        }
-        
-        int res = dinic.max_flow(0, n - 1);
-        cout << res << endl;
-    }
-}
-int main() {
-    m();
+    
+    return flow[t];
 }
 
+
+int max_flow(int s, int t, int n) {
+    int tmp_flow = INF;
+    int res = 0;
+    do {
+        tmp_flow = bfs(s, t, n);
+        res += tmp_flow;
+    }while(tmp_flow);
+}
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
