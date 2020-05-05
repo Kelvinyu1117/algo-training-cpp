d151661a58e84827a7cd07577419c954fa5b7d42
Kelvinyu <wukuwnyu@gmail.com>
2020-05-05 21:24:44 +0800

    dev

---
+++ CS3391/assignment/week8/362.cpp
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+#define maxn 40000
+
+
+using namespace std;
+
+const long long INF  = 1e9+1e5;
+
+struct Edge {
+  int from, to, cap, flow;
+  Edge(int u, int v, int c, int f) : from(u), to(v), cap(c), flow(f) {}
+};
+
+struct Dinic {
+  int n, m, s, t;
+  vector<Edge> edges;
+  vector<int> G[maxn];
+  int d[maxn], cur[maxn];
+  bool vis[maxn];
+
+  void init(int n) {
+    for (int i = 0; i < n; i++) G[i].clear();
+    edges.clear();
+  }
+
+  void AddEdge(int from, int to, int cap) {
+    edges.push_back(Edge(from, to, cap, 0));
+    edges.push_back(Edge(to, from, 0, 0));
+    m = edges.size();
+    G[from].push_back(m - 2);
+    G[to].push_back(m - 1);
+  }
+
+  bool BFS() {
+    memset(vis, 0, sizeof(vis));
+    queue<int> Q;
+    Q.push(s);
+    d[s] = 0;
+    vis[s] = 1;
+    while (!Q.empty()) {
+      int x = Q.front();
+      Q.pop();
+      for (int i = 0; i < G[x].size(); i++) {
+        Edge& e = edges[G[x][i]];
+        if (!vis[e.to] && e.cap > e.flow) {
+          vis[e.to] = 1;
+          d[e.to] = d[x] + 1;
+          Q.push(e.to);
+        }
+      }
+    }
+    return vis[t];
+  }
+
+  int DFS(int x, int a) {
+    if (x == t || a == 0) return a;
+    int flow = 0, f;
+    for (int& i = cur[x]; i < G[x].size(); i++) {
+      Edge& e = edges[G[x][i]];
+      if (d[x] + 1 == d[e.to] && (f = DFS(e.to, min(a, e.cap - e.flow))) > 0) {
+        e.flow += f;
+        edges[G[x][i] ^ 1].flow -= f;
+        flow += f;
+        a -= f;
+        if (a == 0) break;
+      }
+    }
+    return flow;
+  }
+
+  int Maxflow(int s, int t) {
+    this->s = s;
+    this->t = t;
+    int flow = 0;
+    while (BFS()) {
+      memset(cur, 0, sizeof(cur));
+      flow += DFS(s, INF);
+    }
+    return flow;
+  }
+};
+
+int tA[maxn];
+int tB[maxn];
+
+void m() {
+    int L, W, N, D, cnt;
+    
+    cnt = 1;
+    while(cin >> L >> W >> N >> D) {
+        if(!L && !W && !N && !D) return;
+        Dinic dinic;
+        
+        int s = 2 * N;
+        int t = 2 * N + 1;
+        
+        for(int i = 0; i < N; i++)
+            cin >> tA[i] >> tB[i];
+        
+        
+        for(int i = 0; i < N; i++) {
+            
+            dinic.AddEdge(i * 2, i * 2 + 1, 1);
+            
+            if(tB[i] - D <= 0) {
+                dinic.AddEdge(s, i * 2, 1);
+                dinic.AddEdge(i * 2, s, 0);
+            }
+            if(tB[i] + D >= W) {
+                dinic.AddEdge(i * 2 + 1, t, 1);
+                dinic.AddEdge(t, i * 2 + 1, 0);
+            }
+            
+            for (int j = 0; j < i; j++) {
+                if((tA[i] - tA[j]) * (tA[i] - tA[j]) + (tB[i] - tB[j]) * (tB[i] - tB[j]) <= D * D * 4) {
+                    dinic.AddEdge(i * 2 + 1, j * 2, 1);
+					dinic.AddEdge(j * 2 + 1, i * 2, 1);
+                }
+            }
+        }
+        
+        
+        
+        cout << "Case " << cnt << ": " << dinic.Maxflow(s, t) << endl;
+        cnt++;
+    }
+}
+
+int main()
+{
+   
+    m();
+    return 0;
+}
--- useful_snippets/max_flow.cpp
+++ useful_snippets/max_flow.cpp
@@ -1,66 +1,151 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <list>
+#include <string>
 #include <algorithm>
-#define INF 1e9
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+#define MAX_N 40000
+#define INF 1e8+1e5
 using namespace std;
+typedef long long lld;
 
-#define N 300
-int cap[N][N];
-int flow[N];
-int pre[N];
+struct Node
+{
+    vector<lld> adj;
+};
+Node graf[MAX_N];
 
-int bfs(int s, int t, int n) {
-   
-    fill(pre, pre + n, -1);
-    queue<int> q;
-    q.push(s);
-    
-    flow[s] = INF;
-    
-    while(!q.empty()) {
-        int curr = q.front(); q.pop();
-        
-        if(curr == t) break;
-        
-        for(int i = 0; i < n; i++) {
-            if(curr == i) continue;
-            
-            if(pre[i] == -1 && cap[curr][i] > 0) {
-                flow[i] = min(flow[curr], cap[curr][i]);
-                pre[i] = curr;
-                q.push(i);
+struct Edge
+{
+    lld u, v, cap;
+    lld flow;
+};
+vector<Edge> E;
+
+lld v, e;
+lld s, t;
+lld dist[MAX_N];
+lld upTo[MAX_N];
+
+lld idd = 0;
+
+inline bool BFS()
+{
+    for (lld i=1;i<=v;i++) dist[i] = -1;
+    queue<lld> bfs_queue;
+    bfs_queue.push(s);
+    dist[s] = 0;
+    while (!bfs_queue.empty())
+    {
+        lld xt = bfs_queue.front();
+        bfs_queue.pop();
+        for (lld i=0;i<graf[xt].adj.size();i++)
+        {
+            lld currID = graf[xt].adj[i];
+            lld xt1 = E[currID].v;
+            if (dist[xt1] == -1 && E[currID].flow < E[currID].cap)
+            {
+                bfs_queue.push(xt1);
+                dist[xt1] = dist[xt] + 1;
             }
         }
     }
-    
-    if(pre[t] == -1) return 0;
-    
-    int dest = t;
-    
-    while(dest != s) {
-        int from = pre[dest];
-        cap[from][dest] -= flow[t];
-        cap[dest][from] += flow[t];
-        dest = from;
-    }
-    
-    return flow[t];
+    return (dist[t] != -1);
 }
 
+inline lld DFS(lld xt, lld minCap)
+{
+    if (minCap == 0) return 0;
+    if (xt == t) return minCap;
+    while (upTo[xt] < graf[xt].adj.size())
+    {
+        lld currID = graf[xt].adj[upTo[xt]];
+        lld xt1 = E[currID].v;
+        if (dist[xt1] != dist[xt] + 1)
+        {
+            upTo[xt]++;
+            continue;
+        }
+        lld aug = DFS(xt1, min(minCap, E[currID].cap - E[currID].flow));
+        if (aug > 0)
+        {
+            E[currID].flow += aug;
+            if (currID&1) currID--; else currID++;
+            E[currID].flow -= aug;
+            return aug;
+        }
+        upTo[xt]++;
+    }
+    return 0;
+}
 
-int max_flow(int s, int t, int n) {
-    int tmp_flow = INF;
-    int res = 0;
-    do {
-        tmp_flow = bfs(s, t, n);
-        res += tmp_flow;
-    }while(tmp_flow);
+inline lld Dinic()
+{
+    lld flow = 0;
+    while (true)
+    {
+        if (!BFS()) break;
+        for (lld i=1;i<=v;i++) upTo[i] = 0;
+        while (true)
+        {
+            lld currFlow = DFS(s, INF);
+            if (currFlow == 0) break;
+            flow += currFlow;
+        }
+    }
+    return flow;
+}
+
+inline void addEdge(lld u, lld v, lld cap)
+{
+    Edge E1, E2;
+    
+    E1.u = u, E1.v = v, E1.cap = cap, E1.flow = 0;
+    E2.u = v, E2.v = u, E2.cap = 0, E2.flow = 0;
+    
+    graf[u].adj.push_back(idd++);
+    E.push_back(E1);
+    graf[v].adj.push_back(idd++);
+    E.push_back(E2);
+}
+
+inline void clear(lld n) {
+  for(lld i = 1; i <= n; i++) {
+      graf[i].adj.clear();
+  }
+  
+  E.clear();
 }
 
 int main()
 {
-    cout<<"Hello World";
+    lld n, m;
+    
+    while(cin >> n >> m) {
+        
+        v = n;
+        e = m;
+        s = 1;
+        t = n;
+        
+        
+        for(lld i = 0; i < m; i++) {
+            lld u, v, c;
+            cin >> u >> v >> c;
+            
+            addEdge(u, v, c);
+            addEdge(v, u, c);
+        }
+
+        cout << Dinic() << endl;
+    }
 
     return 0;
 }
